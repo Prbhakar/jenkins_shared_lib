5204cf53af1674cd4a6a8e0cebbf881a04af5e05
Prabhakar <prabs003@gmail.com>
2024-12-27 14:03:36 +0530

    Fixed variable issues

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,5 +1,5 @@
 def call(String repoUrl, String branch, String workdir){
-   //def workingDir = "${env.WORKSPACE}"
+   def workingDir = "${workdir}"
    sh "echo received workspace name from pipeline: ${workdir}"
    sh "git clone ${repoUrl} ${workdir}"
    sh "git checkout ${branch}"

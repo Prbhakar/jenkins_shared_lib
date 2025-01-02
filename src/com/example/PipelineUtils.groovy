346897420105a79c81ffba1a036f32e21dfa2cf7
Prabhakar <prabs003@gmail.com>
2025-01-02 11:40:29 +0530

    Updated PipelineUtils

--- src/com/example/PipelineUtils.groovy
+++ src/com/example/PipelineUtils.groovy
@@ -5,6 +5,7 @@ class PipelineUtils {
     static void deleteWorkspace(String workspacePath) {
         println "Cleaning up workspace at: ${workspacePath}"
         println "Cleaning up workspace at src directory: ${workspacePath}"
+        println "Cleaning up workspace at src directory: 02-01-2025"
         // Perform cleanup logic
          sh """
             rm -rf ${workspacePath}/*
